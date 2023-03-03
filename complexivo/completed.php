499c3aa2e60514a7a1ab65ac6c6a7dc5da1f7d12
kevinMotoche123 <71745245+kevin123-web@users.noreply.github.com>
2023-03-03 18:19:30 -0500

    fix and bugs

--- complexivo/completed.php
+++ complexivo/completed.php
@@ -46,6 +46,8 @@ if ($id_trasaction == '') {
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="css/fontawesome-free-6.3.0-web/css/all.min.css">
+
 </head>
 
 <body>
